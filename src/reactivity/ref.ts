2d1e9d4ccf8ec2a025ac3a8267ed13d95484a8b5
xjccc <jing.wang-fe@360che.com>
2022-04-12 09:02:31 +0800

    xj_isRef\unRef实现

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any
   private _rawValue: any
   public dep
+  public __v_isRef = true
   constructor (value) {
     this._rawValue = value
     this._value = convert(value)
@@ -42,4 +43,14 @@ function trackRefValue (ref) {
 
 export function ref (value) {
   return new RefImpl(value)
+}
+
+export function isRef (ref) {
+  return !!ref.__v_isRef
+}
+
+export function unRef (ref) {
+  // 看看是不是ref对象
+  // 是ref对象 -> .value
+  return isRef(ref) ? ref.value : ref
 }
