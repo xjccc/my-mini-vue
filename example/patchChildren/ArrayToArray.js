34d8dee91cf39c242807b777afa1923b431067cf
xjccc <546534045@qq.com>
2023-06-05 00:33:27 +0800

    feat: 最长递增子序列

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -115,12 +115,82 @@ import { h, ref } from '../../lib/guide-mini-vue.esm.js'
 // a b c e d f g
 // a b e c f g
 // 中间部分，老的比新的多，那么多出来的可以直接循环的时候干掉
+// const prevChildren = [
+//   h('p', {key: 'A'}, "A"),
+//   h('p', {key: 'B'}, "B"),
+//   h('p', {key: 'C', id: "c-prev"}, "C"),
+//   h('p', {key: 'E'}, "E"),
+//   h('p', {key: 'D'}, "D"),
+//   h('p', {key: 'F'}, "F"),
+//   h('p', {key: 'G'}, "G"),
+// ]
+
+// const nextChildren = [
+//   h('p', {key: 'A'}, "A"),
+//   h('p', {key: 'B'}, "B"),
+//   h('p', {key: 'E'}, "E"),
+//   h('p', {key: 'C', id: "c-next"}, "C"),
+//   h('p', {key: 'F'}, "F"),
+//   h('p', {key: 'G'}, "G"),
+// ]
+// 2 移动 （节点存在于新的和老的里面，但是位置变了）
+// 2.1
+// a b c d e f g
+// a b e c d f g
+// 最长子序列[1, 2]
+// const prevChildren = [
+//   h('p', {key: 'A'}, "A"),
+//   h('p', {key: 'B'}, "B"),
+//   h('p', {key: 'C', id: "c-prev"}, "C"),
+//   h('p', {key: 'D'}, "D"),
+//   h('p', {key: 'E'}, "E"),
+//   h('p', {key: 'F'}, "F"),
+//   h('p', {key: 'G'}, "G"),
+// ]
+
+// const nextChildren = [
+//   h('p', {key: 'A'}, "A"),
+//   h('p', {key: 'B'}, "B"),
+//   h('p', {key: 'E'}, "E"),
+//   h('p', {key: 'C', id: "c-next"}, "C"),
+//   h('p', {key: 'D'}, "D"),
+//   h('p', {key: 'F'}, "F"),
+//   h('p', {key: 'G'}, "G"),
+// ]
+
+// 3. 创建新节点
+// a b c e f g
+// a b e c d f g
+// d节点在老节点中不存在，新的里面存在，所以需要创建
+// const prevChildren = [
+//   h('p', {key: 'A'}, "A"),
+//   h('p', {key: 'B'}, "B"),
+//   h('p', {key: 'C', id: "c-prev"}, "C"),
+//   h('p', {key: 'E'}, "E"),
+//   h('p', {key: 'F'}, "F"),
+//   h('p', {key: 'G'}, "G"),
+// ]
+
+// const nextChildren = [
+//   h('p', {key: 'A'}, "A"),
+//   h('p', {key: 'B'}, "B"),
+//   h('p', {key: 'E'}, "E"),
+//   h('p', {key: 'C', id: "c-next"}, "C"),
+//   h('p', {key: 'D'}, "D"),
+//   h('p', {key: 'F'}, "F"),
+//   h('p', {key: 'G'}, "G"),
+// ]
+
+// 综合例子
+// a b c d e z f g
+// a b d c y e f g
 const prevChildren = [
   h('p', {key: 'A'}, "A"),
   h('p', {key: 'B'}, "B"),
   h('p', {key: 'C', id: "c-prev"}, "C"),
-  h('p', {key: 'E'}, "E"),
   h('p', {key: 'D'}, "D"),
+  h('p', {key: 'E'}, "E"),
+  h('p', {key: 'Z'}, "Z"),
   h('p', {key: 'F'}, "F"),
   h('p', {key: 'G'}, "G"),
 ]
@@ -128,8 +198,10 @@ const prevChildren = [
 const nextChildren = [
   h('p', {key: 'A'}, "A"),
   h('p', {key: 'B'}, "B"),
-  h('p', {key: 'E'}, "E"),
+  h('p', {key: 'D'}, "D"),
   h('p', {key: 'C', id: "c-next"}, "C"),
+  h('p', {key: 'Y'}, "Y"),
+  h('p', {key: 'E'}, "E"),
   h('p', {key: 'F'}, "F"),
   h('p', {key: 'G'}, "G"),
 ]
